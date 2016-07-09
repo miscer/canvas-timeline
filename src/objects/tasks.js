884f831cfddedc88dfd2e5eb6e8b708260e1cfa1
Michal Miškerník <michal@miskernik.sk>
2016-07-09 22:25:11 +0200

    Fixed overflowing task titles

--- src/objects/tasks.js
+++ src/objects/tasks.js
@@ -6,6 +6,15 @@ import {
 import { getColorFromId } from '../utils/color_map';
 
 const TASK_HEIGHT = 43;
+const PIXEL_RATIO = window.devicePixelRatio || 1;
+
+const TEXT_CANVAS = document.createElement('canvas');
+TEXT_CANVAS.width = 1000;
+TEXT_CANVAS.height = 50;
+
+const TEXT_CONTEXT = TEXT_CANVAS.getContext('2d');
+TEXT_CONTEXT.scale(PIXEL_RATIO, PIXEL_RATIO);
+TEXT_CONTEXT.textBaseline = 'top';
 
 export const render = (state) => {
   const ctx = contextSelector(state);
@@ -14,7 +23,6 @@ export const render = (state) => {
   const scroll = state.scroll;
 
   ctx.save();
-  ctx.textBaseline = 'top';
 
   for (const item of tasks) {
     renderTask(
@@ -31,22 +39,55 @@ export const render = (state) => {
 
 const renderTask = (state, task, width, x, y) => {
   const ctx = contextSelector(state);
+  const text = TEXT_CANVAS;
 
   ctx.fillStyle = getColorFromId(task.color_id);
   ctx.fillRect(x, y, width - 2, TASK_HEIGHT);
 
-  ctx.fillStyle = '#000';
-  ctx.font = 'bold 14px Avenir';
+  prepareText(
+    task.name,
+    width - 12,
+    '#000', 'bold 14px Avenir'
+  );
 
-  ctx.fillText(task.name, x + 5, y + 5);
+  renderText(ctx, x + 5, y + 5);
 
   if (task.project != null) {
-    ctx.fillStyle = 'rgba(34, 34, 34, 0.5)';
-    ctx.font = '14px Avenir';
+    prepareText(
+      task.project.name,
+      width - 12,
+      'rgba(34, 34, 34, 0.5)',
+      '14px Avenir'
+    );
 
-    ctx.fillText(task.project.name, x + 5, y + 22);
+    renderText(ctx, x + 5, y + 22);
   }
 
   ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
   ctx.fillRect(x, y + 41, width - 2, 2);
+};
+
+const prepareText = (text, maxWidth, color, font) => {
+  const ctx = TEXT_CONTEXT;
+  const {width, height} = TEXT_CANVAS;
+
+  ctx.clearRect(0, 0, width, height);
+
+  ctx.font = font;
+  ctx.fillStyle = color;
+  ctx.fillText(text, 0, 0);
+
+  ctx.clearRect(maxWidth, 0, width - maxWidth, height);
+};
+
+const renderText = (ctx, x, y) => {
+  const canvas = TEXT_CANVAS;
+
+
+  ctx.drawImage(
+    canvas,
+    x, y,
+    canvas.width / PIXEL_RATIO,
+    canvas.height / PIXEL_RATIO
+  );
 };
