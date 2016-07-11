048e6611db5119ba927cbb6d7fd816799febbebd
Michal Miškerník <michal@miskernik.sk>
2016-07-11 09:14:05 +0200

    Don't change canvas size when scrolling

--- src/canvas.js
+++ src/canvas.js
@@ -5,12 +5,19 @@ export default (store) => {
   const element = document.querySelector('canvas');
   store.dispatch(setCanvas(element));
 
+  let size = null;
+
   const updateSize = () => {
-    const {width, height} = store.getState().size;
-    const {element} = store.getState().canvas;
-    element.width = width;
-    element.height = height;
-    autoscale(element);
+    const state = store.getState();
+
+    if (state.size !== size) {
+      size = state.size;
+
+      const {element} = state.canvas;
+      element.width = size.width;
+      element.height = size.height;
+      autoscale(element);
+    }
   };
 
   store.subscribe(updateSize);
